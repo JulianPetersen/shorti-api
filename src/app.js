89d4c881958402faf9cda5fb98d09d5bfacf998d
JulianPetersen <91999222+JulianPetersen@users.noreply.github.com>
2023-02-24 15:55:39 -0300

    actualizacion de version

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ app.get('/', (req,res)=> {
     res.json({
         author:'Frigg',
         description:"API made by frigg, for shorti App.",
-        version:"1.0.0"
+        version:"1.0.1"
     })
 })
 
